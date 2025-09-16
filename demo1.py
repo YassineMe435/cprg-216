88060c0c7395ae00c06f66df61ac00a5e5278ab4
YassineMe435 <hammoudamessaoudi123@gmail.com>
2025-09-16 09:44:32 -0700

    Test modify

---
+++ demo1.py
@@ -0,0 +1,8 @@
+x = 3
+y = 4.5
+message = "hello"
+msg = "hola"
+
+old = True
+young = False
+z = 5
---
+++ mydemo0.py
@@ -0,0 +1,7 @@
+Number = 8
+Birthday = 24
+Message = "What's up !"
+decimal_number = 6.5
+Yes = True
+No = False
+print(Number, Birthday, Message, decimal_number, Yes, No)
